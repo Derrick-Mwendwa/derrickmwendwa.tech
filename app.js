3336fae5bd81630725a9c01309372cdfe56790c8
Derek Mwendwa <derekmwendwa15@gmail.com>
2023-05-06 02:32:32 +0300

    created a socket server

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-const http = require('http');
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
 
-http.createServer( function (req, res) {
-	res.write("On the way to being a fullsnack engineer...almost there Derrick");
-	res.end();
-}).listen(3000);
+app.get('/', function(req, res) {
+        res.sendFile('index.html', {root: __dirname});
+});
 
-console.log("server started on port 3000");
+server.on('request', app);
+server.listen(3000, function () { console.log('Listening on 3000'); });
+
+/** Begin websocket */
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function connection(ws) {
+        const numClients = wss.clients.size;
+        console.log(`Total clients: ${numClients}`);
+
+        wss.broadcast(`Current visitors: ${numClients}`);
+
+        if (ws.readyState === ws.OPEN) {
+                ws.send('Welcome to my server!');
+        }
+
+        ws.on('close', function close() {
+                wss.broadcast(`Current visitors: ${numClients}`);
+                console.log('A client has disconnected');
+        });
+});
+
+wss.broadcast = function broadcast(data) {
+        wss.clients.forEach(function each(client) {
+                client.send(data);
+        });
+}
